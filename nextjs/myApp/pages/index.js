bc9c9d7529435465c4f923b1c2d48ed0cfc5ff49
happy4 <happyl4lu@gmail.com>
2021-12-08 12:13:00 +0200

    Handle REST API requests

--- nextjs/myApp/pages/index.js
+++ nextjs/myApp/pages/index.js
@@ -1,39 +1,64 @@
 import React from 'react';
-import DigitalClock from '../src/DigitalClock';
+import axios from 'axios';
+import Link from 'next/Link';
 
 class Index extends React.Component {
 
+  static async getInitialProps () {
+    const promise = axios.get('http://localhost:4000/speakers').
+      then(response => {
+        return {
+          hasError: false,
+          speakerData: response.data
+        }
+      })
+      .catch(error => {
+        return {
+          hasError: true,
+          message: error.message
+        }
+      });
+      return promise;
+  }
+
   constructor(props) {
     super(props);
+
+    const {
+      speakerData,
+      hasError,
+      message
+    } = props;
+
     this.state = {
-      time: new Date().toLocaleString()
+      speakerData,
+      hasError,
+      message
     };
   }
 
-  tick() {
-    this.setState(() => {
-      return ({
-        time: new Date().toLocaleString()
-      });
-    });
-  }
-
   componentDidMount() {
-    this.interval = setInterval(() => this.tick(), 1000);
   }
 
   render() {
-    const {
-      time
-    } = this.state;
-
     return (
-      <DigitalClock time={time}> </DigitalClock>
+      <>
+        <Link href="/sessions">
+          <a>SESSIONS</a>
+        </Link>
+        <ul>
+          {this.state.speakerData.map((speaker) =>
+              <li key={speaker.id}>
+                {speaker.firstName}
+              </li>
+            )
+          }
+        </ul>
+      </>
     )
   }
 
   componentWillUnmount() {
-    this.clearInterval(this.interval);
   }
 };
 
